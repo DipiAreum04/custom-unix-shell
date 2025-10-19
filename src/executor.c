2506a54e8254f2c2c7bcd862bb34fe1efbaac767
DipiAreum04 <di_sinh@live.concordia.ca>
2025-10-18 20:43:44 -0400

    Simple Command Execution with Background Control Implemented

--- src/executor.c
+++ src/executor.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/fcntl.h>
+#include <sys/wait.h>
 
 #include "parser.h"
 
@@ -14,7 +15,31 @@
  * @param bg
  * @return
  */
-int execute_command(char* cmd, char** args, int in, int out, int bg) {
+int execute_command(char* cmd, char** args, int bg) {
+    
+    pid_t pid = fork();
+    if (pid < 0) {
+		// fork failed
+		perror("fork failed\n");
+		return EXIT_FAILURE;
+    }
+    else if(pid == 0) {
+		// child process
+
+        // Replace the child process with a new program to execute the command
+        execvp(cmd, args);
+
+        // If execvp returns, an error occurred
+        perror("execvp failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (bg == 0) { 
+        // if bg not set, shell waits until the command execution completes before entering new command
+        wait(NULL);
+    }
+    // if bg is 1, shell does not wait for command execution completion
+
     return EXIT_SUCCESS;
 }
 
@@ -25,19 +50,18 @@ int execute_command(char* cmd, char** args, int in, int out, int bg) {
  */
 int execute(struct cmdline *l){
     printf("TODO: Execute the command\n");
-    int i,j;
+    int i;
     if (l->in) printf("in: %s\n", l->in);
     if (l->out) printf("out: %s\n", l->out);
     if (l->bg) printf("background (&)\n");
 
     /* Display each command of the pipe */
     for (i=0; l->seq[i]!=0; i++) {
-        char **cmd = l->seq[i];
-        printf("seq[%d]: ", i);
-        for (j=0; cmd[j]!=0; j++) {
-            printf("'%s' ", cmd[j]);
-        }
-        printf("\n");
+        char** args = l->seq[i];    // Command arguments
+        char* cmd = args[0];   // Command name
+
+        return execute_command(cmd, args, l->bg);
     }
+
     return EXIT_SUCCESS;
 }
